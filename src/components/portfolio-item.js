1440d82354a6b541ee5f0141708850687cd425fa
Ryan Scotton <ryan.scotton@gmail.com>
2019-04-16 22:00:12 -0400

    Cleanup

--- src/components/portfolio-item.js
+++ src/components/portfolio-item.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default props => (
   <div className="portfolio-item">
     <h3>{props.title}</h3>
-    <a href={props.url} target="_blank">{props.url}</a>
+    <a href={props.url} target="_blank" rel="noopener noreferrer">{props.url}</a>
   </div>
 );
