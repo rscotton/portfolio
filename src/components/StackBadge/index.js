b51fcc8f78f63c48f039e0cf51f08a2dfccd1148
Ryan Scotton <ryan.scotton@gmail.com>
2019-12-16 23:23:05 -0500

    cleanup

--- src/components/StackBadge/index.js
+++ src/components/StackBadge/index.js
@@ -3,17 +3,14 @@ import React from 'react';
 import cn from 'classnames';
 import componentStyles from './styles.module.scss';
 
-export default ({ stackItem }) => {
-  console.log(componentStyles);
-  return (
-    <div
-      className={cn(
-        'h6',
-        componentStyles['stack-badge'],
-        componentStyles[`stack-badge-${slugify(stackItem)}`]
-      )}
-    >
-      {stackItem}
-    </div>
-  );
-};
+export default ({ stackItem }) => (
+  <div
+    className={cn(
+      'h6',
+      componentStyles['stack-badge'],
+      componentStyles[`stack-badge-${slugify(stackItem)}`]
+    )}
+  >
+    {stackItem}
+  </div>
+);
