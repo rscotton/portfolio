16c7bf36673500118f42ab1c0b53f6ddb27d9d82
Ryan Scotton <ryan.scotton@gmail.com>
2019-12-18 01:56:49 -0500

    Conver remaining StaticQuery components to hooks

--- src/components/GlobalHeader/components/MainMenu/index.tsx
+++ src/components/GlobalHeader/components/MainMenu/index.tsx
@@ -1,33 +1,33 @@
-import { Link, StaticQuery, graphql } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import React from 'react';
 import { MenuItemNode } from '@src/types';
 
-const GlobalHeader: React.FC = () => (
-  <ul className="menu-primary">
-    <StaticQuery
-      query={graphql`
-        query MenuMainQuery {
-          allMenuJson {
-            edges {
-              node {
-                href
-                label
-              }
-            }
+const GlobalHeader: React.FC = () => {
+  const mainMenuQuery = useStaticQuery(graphql`
+    query MenuMainQuery {
+      allMenuJson {
+        edges {
+          node {
+            href
+            label
           }
         }
-      `}
-      render={data =>
-        data.allMenuJson.edges
-          .map((edge: any) => edge.node)
-          .map((menuItem: MenuItemNode) => (
-            <li key={menuItem.label} className="nav-item nav-item-primary">
-              <Link to={menuItem.href}>{menuItem.label}</Link>
-            </li>
-          ))
       }
-    />
-  </ul>
-);
+    }
+  `);
+  const menuItems = mainMenuQuery.allMenuJson.edges.map(
+    (edge: any) => edge.node
+  );
+
+  return (
+    <ul className="menu-primary">
+      {menuItems.map((menuItem: MenuItemNode) => (
+        <li key={menuItem.label} className="nav-item nav-item-primary">
+          <Link to={menuItem.href}>{menuItem.label}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default GlobalHeader;
--- src/components/PortfolioItems/index.tsx
+++ src/components/PortfolioItems/index.tsx
@@ -1,44 +1,45 @@
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import PortfolioSection from '@src/components/PortfolioSection';
 import React from 'react';
 import { PortfolioItemNode } from '@src/types';
 
-const PortfolioItems: React.FC = () => (
-  <div id="portfolio" className="portfolio portfolio-items">
-    <StaticQuery
-      query={graphql`
-        query PortfolioMainQuery {
-          allMarkdownRemark(
-            filter: { frontmatter: { published: { eq: true } } }
-            sort: { order: ASC, fields: [frontmatter___weight] }
-          ) {
-            edges {
-              node {
-                frontmatter {
-                  path
-                  title
-                  website
-                  stack
-                }
-                html
-              }
+const PortfolioItems: React.FC = () => {
+  const portfolioItemsQuery = useStaticQuery(graphql`
+    query PortfolioMainQuery {
+      allMarkdownRemark(
+        filter: { frontmatter: { published: { eq: true } } }
+        sort: { order: ASC, fields: [frontmatter___weight] }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+              website
+              stack
             }
+            html
           }
         }
-      `}
-      render={data =>
-        data.allMarkdownRemark.edges
-          .map((edge: any) => edge.node)
-          .map((portfolioItem: PortfolioItemNode) => (
-            <PortfolioSection
-              key={portfolioItem.frontmatter.path}
-              {...portfolioItem.frontmatter}
-              content={portfolioItem.html}
-            />
-          ))
       }
-    />
-  </div>
-);
+    }
+  `);
+  const portfolioItems = portfolioItemsQuery.allMarkdownRemark.edges.map(
+    (edge: any) => edge.node
+  );
+
+  return (
+    <div id="portfolio" className="portfolio portfolio-items">
+      {portfolioItems.map((portfolioItem: PortfolioItemNode) => (
+        <PortfolioSection
+          key={portfolioItem.frontmatter.path}
+          {...portfolioItem.frontmatter}
+          content={portfolioItem.html}
+        />
+      ))}
+      />
+    </div>
+  );
+};
 
 export default PortfolioItems;
