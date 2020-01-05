a7881d0da39dccda5fb9f09fd31351a71b56a03b
Ryan Scotton <ryan.scotton@gmail.com>
2020-01-05 03:05:00 -0500

    Add offsets to Col component

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -4,6 +4,7 @@ import Container from '@components/Container';
 import componentStyles from './styles.module.scss';
 
 type GridColumnWidth = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+type GridColumnOffset = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
 
 interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
   xs?: GridColumnWidth;
@@ -11,6 +12,11 @@ interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
   md?: GridColumnWidth;
   lg?: GridColumnWidth;
   xl?: GridColumnWidth;
+  xsOffset?: GridColumnOffset;
+  smOffset?: GridColumnOffset;
+  mdOffset?: GridColumnOffset;
+  lgOffset?: GridColumnOffset;
+  xlOffset?: GridColumnOffset;
 }
 
 export const Col: React.FC<ColProps> = ({
@@ -19,22 +25,32 @@ export const Col: React.FC<ColProps> = ({
   md,
   lg,
   xl,
+  xsOffset,
+  smOffset,
+  mdOffset,
+  lgOffset,
+  xlOffset,
   children,
   ...rest
 }) => {
   const colOnly = !xs && !sm && !md && !lg && !xl;
-  const className = cn(
+  const colClasses = cn(
     componentStyles['col-base'],
     colOnly && componentStyles.col,
     xs && componentStyles[`col-${xs}`],
     sm && componentStyles[`col-sm-${sm}`],
     md && componentStyles[`col-md-${md}`],
     lg && componentStyles[`col-lg-${lg}`],
-    xl && componentStyles[`col-xl-${xl}`]
+    xl && componentStyles[`col-xl-${xl}`],
+    typeof xsOffset !== undefined && componentStyles[`offset-${xsOffset}`],
+    typeof smOffset !== undefined && componentStyles[`offset-sm-${smOffset}`],
+    typeof mdOffset !== undefined && componentStyles[`offset-md-${mdOffset}`],
+    typeof lgOffset !== undefined && componentStyles[`offset-lg-${lgOffset}`],
+    typeof xlOffset !== undefined && componentStyles[`offset-xl-${xlOffset}`]
   );
 
   return (
-    <div className={className} {...rest}>
+    <div className={colClasses} {...rest}>
       {children}
     </div>
   );
